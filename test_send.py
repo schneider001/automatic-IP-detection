f48bb73e507711bca3f4051e523d5d19e29b9bfa
schneider001 <69435543+schneider001@users.noreply.github.com>
2021-04-13 03:50:30 +0300

    Add files via upload

---
+++ test_send.py
@@ -0,0 +1,12 @@
+import pika
+
+credentials = pika.PlainCredentials('postman', '11362266')
+parameters = pika.ConnectionParameters('192.168.0.3', credentials=credentials) 
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+print ("[x] Sent 'Hello World!")
+connection.close()
