f3f66d50d6343b528c69d197b5ef65b6fdc6ce79
schneider001 <69435543+schneider001@users.noreply.github.com>
2021-05-17 19:02:35 +0300

    Create test_receive.py

---
+++ RMQ common communication/test_receive.py
@@ -0,0 +1,17 @@
+import pika
+
+credentials = pika.PlainCredentials('username', 'password')
+parameters = pika.ConnectionParameters('192.168.0.3', credentials=credentials) 
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+print ('[*] Waiting for messages. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] Received %r" % body)
+
+channel.basic_consume('hello', callback, auto_ack=True)
+
+channel.start_consuming()
